fde5aeda7858823fcf7ee1e4e5efb1af7f0d8c22
fharhanamrin <fharhanamrin028@gmail.com>
2025-01-19 15:42:10 +0700

    final belajar pertemuan 1 sampai 10 done

--- pertemuan_7/Pertemuan_7.java
+++ pertemuan_7/Pertemuan_7.java
@@ -1,4 +1,5 @@
 package pertemuan_7;
+
 import java.io.*;
 // import java.io.FileReader;
 // import java.io.IOException;
@@ -10,19 +11,35 @@ public class Pertemuan_7 {
     // TOPIk : Stream
     public static void main(String[] args) {
         String x = "";
-        int harga=0, jumlah=0, n=0;
+        int harga = 0, jumlah = 0, n = 0;
         try {
-            FileInputStream fs = new FileInputStream("/Users/users/Documents/semester3/materi_java_semester_3/pertemuan_7/harga.txt");
+            String filePath = "/Users/users/Documents/semester3/materi_java_semester_3/pertemuan_7/harga.txt";
+
+            FileInputStream fs = new FileInputStream(filePath);
             DataInputStream di = new DataInputStream(fs);
             // System.out.println("Working Directory = " + System.getProperty("user.dir"));
-            while (di.available()!=0) {
+            while (di.available() != 0) {
                 // String data = di.readLine();
                 // System.out.println(data);
                 x = di.readLine();
                 // harga=Integer.parseInt(x.substring(1,5));
                 // jumlah = harga;
-                n= n+1;
-                System.out.println("Harga hari ke "+n+" : "+ x);
+                n = n + 1;
+                System.out.println("Harga hari ke " + n + " : " + x);
+            }
+
+            String dataToAppend = "Data baru yang ingin ditambahkan";
+
+            try (FileWriter fw = new FileWriter(filePath, true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    PrintWriter pw = new PrintWriter(bw)) {
+
+                // Menambahkan data baru di akhir file
+                
+                pw.println(dataToAppend);
+                System.out.println("Data berhasil ditambahkan ke file.");
+            } catch (IOException e) {
+                System.out.println("Terjadi kesalahan: " + e.getMessage());
             }
             di.close();
         } catch (Exception e) {
@@ -31,7 +48,11 @@ public class Pertemuan_7 {
         }
 
         try {
-            FileOutputStream out = new FileOutputStream("/Users/users/Documents/semester3/materi_java_semester_3/pertemuan_7/harga_out.txt");//akan kebuat otomatis jika file tidak ada
+            FileOutputStream out = new FileOutputStream(
+                    "/Users/users/Documents/semester3/materi_java_semester_3/pertemuan_7/harga_out.txt");// akan kebuat
+                                                                                                         // otomatis
+                                                                                                         // jika file
+                                                                                                         // tidak ada
             PrintStream p = new PrintStream(out);
             p.println("Fharhhan 1|Teknik Informatika|Semester 3");
             p.println("Fharhhan 2|Teknik Informatika|Semester 3");
@@ -43,6 +64,6 @@ public class Pertemuan_7 {
             // TODO: handle exception
             System.out.println(e);
         }
-        
+
     }
 }
